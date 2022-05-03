1affc0bf01a864f4be3018070e3ff9341563ede7
Levi.Tracy <Levi.Tracy@IVP-LDD4KXMG3>
2022-05-03 14:02:34 -0400

    Add project files.

---
+++ EvenOrOdd/Program.cs
@@ -0,0 +1,25 @@
+﻿Console.Write("Enter an integer: ");
+var answer = Console.ReadLine();
+var theNbr = Convert.ToInt32(answer);
+
+while (theNbr != 0)
+
+    {
+    if (theNbr % 2 == 0)
+        //Modoulous used to confirm divisible by 2, if 0 that's even
+        {
+        Console.WriteLine($"The number {theNbr} is even.");
+        }
+    else
+        {
+        Console.WriteLine($"The number {theNbr} is Odd.");
+        }
+    Console.Write("Enter an integer: ");
+    answer = Console.ReadLine();
+    theNbr = Convert.ToInt32(answer);
+    //these statments have to be in a while loop, so it can continue to ask the user if they want to be asked to do it again
+    }
+
+
+
+Console.WriteLine("Done ...");
